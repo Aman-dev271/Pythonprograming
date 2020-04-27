605fd1951310fadbdbb530f61618ecf1ba8c9745
Aman-dev271 <59610862+Aman-dev271@users.noreply.github.com>
2020-04-27 09:09:28 +0530

    Add files via upload

---
+++ 1stInput_Take.py
@@ -0,0 +1,19 @@
+# input python by default it take an input as string 
+print("enter your F_name")
+F_name = input()
+print("second S_name")
+S_name = input()
+print("your addition is" ,(F_name)+(S_name))
+
+# if we want to change or convert it to integer to to that
+print("enter your F_number")
+F_number = input()
+print("second S_number")
+S_number = input()
+print("your addition is" ,int(F_number)+int(S_number))
+
+
+# we can also take input as give below
+F_number = int(input("enter the first_number"))
+S_number = int(input("enter the second_number"))
+print("your addition is",F_number+S_number)
