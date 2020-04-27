4855cb34c0f2e7f1858e3c7319c56a249c7e6fbb
Aman-dev271 <59610862+Aman-dev271@users.noreply.github.com>
2020-04-27 09:16:16 +0530

    Add files via upload

---
+++ 7thrange.py
@@ -0,0 +1,25 @@
+# the range function genrates the value from the fly 
+for x in range(2,100,3):
+    '''
+    first parameter is '2' for start the range here i start the range from '2' 
+    and the second parameter i.e '100' mean ending  and the '3' is for step 
+    that means is starts as
+     2
+     5
+     8'''
+
+    print(x)
+
+# we can work with the range function with that way 
+for i in range(1000):
+    print(i)
+
+
+# we can work with the range function with that way 
+# with two parameters 
+for a in range(1,1000):#that mean start from the '1' and ends upto 1000
+    print(a)
+
+
+
+# so this is the range function
